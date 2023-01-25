aa942e576691bf9ee57f909628e31ed6db886fed
mim2041 <suraiyamim338@gmail.com>
2023-01-25 23:25:47 +0600

    exploring JavaScript types, operations

---
+++ types.js
@@ -0,0 +1,14 @@
+var price = 100;
+// console.log(typeof price);
+var price = '100';
+// console.log(typeof price);
+var price = true;
+// console.log(typeof price);
+var romantic;
+// console.log(typeof romantic);
+
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(2);
+console.log(sum);
