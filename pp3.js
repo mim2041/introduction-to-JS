405769b12646330e5f6e4f358566c02d23a9b237
mim2041 <suraiyamim338@gmail.com>
2023-01-26 00:00:04 +0600

    more practice

---
+++ pp3.js
@@ -0,0 +1,4 @@
+var firstLine = "I am going to be";
+var lastLine = "an awesome web developer";
+var line = firstLine + " " + lastLine;
+console.log(line);
---
+++ pp4.js
@@ -0,0 +1,3 @@
+var number = 119;
+const remainder = number % 5;
+console.log(remainder);
